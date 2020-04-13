956c003ca1909d851af02f92cc77a2101faa4d8d
pradeep misra <pradeep.misra@kognitiv.com>
2020-04-13 21:48:40 +0530

    Updated the newly added React Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person1 from './Person/Person.js'
+import Person from './Person/Person.js'
 
 // function App() {
 //   return (
@@ -17,7 +17,11 @@ class App extends Component {
       <div className="App">
         <hi>Hi, this is my first react demo application</hi>
         <p>This is really working</p>
-        <Person1 />
+        <Person name="Rahav" age="28"/>
+        <Person name="Sunil" age="29"/>
+        <Person name="Satya" age="30"/>
+        <Person name="Praveen" age="31"/>
+        <Person name="Seema" age="32"/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Does this works?'));
