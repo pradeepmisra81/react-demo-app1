dc5f93589b0940e32d87a654255ebb436e36cf62
pradeep misra <pradeep.misra@kognitiv.com>
2020-04-13 21:05:00 +0530

    removed the example javascript files

--- src/examples/arrayDestructuring.js
+++
@@ -1,9 +0,0 @@
-const numbers = [1,2,3];
-
-[num1,num2] = numbers;
-console.log("num1:",num1);
-console.log("numm2:",num2);
-
-[num1,,num2] = numbers;
-console.log("num1:",num1);
-console.log("numm2:",num2);
