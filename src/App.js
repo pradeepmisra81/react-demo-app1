c03617c2294f9ac3bfa7e8c7e96f51b1a656c01f
pradeep misra <pradeep.misra@kognitiv.com>
2020-04-25 19:42:07 +0530

    Implemented outputting Lists

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons:!doesShow})
+    this.setState({ showPersons: !doesShow })
   }
 
   render() {
@@ -64,25 +64,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            >
-            </Person>
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Ramakant')}
-              changed={this.nameChangedHandler}
-            >
-              I belong to Mathura
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            >
-            </Person>
-          </div>
+          {this.state.persons.map(person => {
+            return <Person
+              name={person.name}
+              age={person.age}
+            />
+          })}
+        </div>
       );
     }
 
@@ -95,7 +83,7 @@ class App extends Component {
           onClick={this.togglePersonsHandler}
         >
           Toggle Persons
-        </Button> 
+        </Button>
         {persons}
       </div>
     );
