97b711e96ce694a500d2458ade7e546878fc0151
pradeep misra <pradeep.misra@kognitiv.com>
2020-04-28 20:38:28 +0530

    implemented setting styles dynamically

--- src/App.js
+++ src/App.js
@@ -67,7 +67,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -90,6 +91,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
 
     return (
