d25a435d64794712807412aaed85dd8e19b4e094
pradeep misra <pradeep.misra@kognitiv.com>
2020-05-01 00:22:35 +0530

    updated redux-basics

--- src/redux-basics.js
+++ src/redux-basics.js
@@ -1,5 +1,14 @@
+const redux = require('react-redux');
+const createStore = redux.createStore;
+
+// reducer
+const rootReducer = (state, action) => {
+    return state;
+}
+
 // store
+const store = createStore(rootReducer);
 
 // dispatching action
 
-// reducer
+// subscription 
