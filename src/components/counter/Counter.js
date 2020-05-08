b92746751bbdf53adf1fbead22a473d93c3cb9a4
pradeep misra <pradeep.misra@kognitiv.com>
2020-05-08 15:20:02 +0530

    written Counter component

---
+++ src/components/counter/Counter.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function Counter ({increment, decrement, count}) {
+    return (
+        <div>
+            <button onClick={increment}>+</button>
+            <span>{count}</span>
+            <button onClick={decrement}>-</button>
+        </div>
+    );
+}
+
+export default connect(state => ({
+    count: state.count
+}),
+dispatch => ({
+    increment: () => dispatch({type: "INCREMENT"}),
+    decrement: () => dispatch({type: "DECREMENT"})
+})
+)(Counter);
